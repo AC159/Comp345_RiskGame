5e3f816ba649cddb050f90976d6a6642afe2c334
carloako <carloestoesta@gmail.com>
2022-02-01 14:24:32 -0500

    Revert "Revert "Added destructor and other small changes""
    This reverts commit 5640b4143105646d99884df2181ac9cc666f7cc4.

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -5,42 +5,91 @@
 #include "Player.h"
 
 
-Player::Player(){}
-
-//constructor for Player class to initialize the name and the collection of territories, cards, and orders
-Player::Player(string newName){
+Player::Player(){
+    name = "No Name";
     territories = new map<int, Territory*>;
     cards = new list<TestCards*>;
     orders = new list<TestOrders*>;
-    name = newName;
 }
 
+
+//constructor for Player class to initialize the name and the collection of territories, cards, and orders
+Player::Player(string newName){
+    name = newName;
+    territories = new map<int, Territory*>;
+    cards = new list<TestCards*>;
+    orders = new list<TestOrders*>;
+}
+
+
+// copy constructor(not tested)
 Player::Player(const Player &player){
-    map<int, Territory*>* otherT = new map<int, Territory*>;
+    this->name = player.name;
+    // delete, create a new mapping of territories and copy all territories from other player
+    delete territories;
+    territories = new map<int, Territory*>;
     for(map<int,Territory*>::iterator it = player.territories->begin(); it != player.territories->end(); it++){
-        otherT->insert(pair<int, Territory*>(it->second->countryNumber, it->second));
-        cout << (it->second->name) << endl;
+        territories->insert(pair<int, Territory*>(it->second->countryNumber, it->second));
+        // cout << (it->second->name) << endl;
+    }
+    // delete, create a new list of cards and copy all cards from other player
+    delete cards;
+    cards = new list<TestCards*>;
+    for(list<TestCards*>::iterator it = player.cards->begin(); it != player.cards->end(); it++){
+        list<TestCards*>::iterator itCards = cards->begin();
+        cards->insert(itCards, *it);
+        itCards++;
+        // cout << (it->second->name) << endl;
+    }
+    // delete, create a new list of orders and copy all orders from other player
+    delete orders;
+    orders = new list<TestOrders*>;
+    for(list<TestOrders*>::iterator it = player.orders->begin(); it != player.orders->end(); it++){
+        list<TestOrders*>::iterator itOrders = orders->begin();
+        orders->insert(itOrders, *it);
+        itOrders++;
+        // cout << (it->second->name) << endl;
     }
 }
 
+
+// destructor
+Player::~Player(){
+    delete territories;
+    delete cards;
+    delete orders;
+}
+
+
+// assignment operator(not tested)
 Player &Player::operator=(const Player &player){
-
     if (this == &player) return *this;
-
+    
     this->name = player.name;
     delete this->territories;
-    this->territories = player.territories;
-    // for(map<int, Territory*>::iterator it = player.territories->begin(); it != player.territories->end(); it++){
-    //     this->territories->insert(it->first,it->second);
-    // }
-    delete this->orders;
-    this->orders = player.orders;
+    this->territories = new map<int, Territory*>;
+    for(map<int, Territory*>::iterator it = player.territories->begin(); it != player.territories->end(); it++){
+        this->territories->insert(pair<int, Territory*>(it->second->countryNumber, it->second);
+    }
     delete this->cards;
-    this->cards = player.cards;
+    this->cards = new list<TestCards*>;
+    for(list<TestCards*>::iterator it = player.cards->begin(); it != player.cards->end(); it++){
+        list<TestCards*>::iterator itCards = cards->begin();
+        this->cards->insert(itCards, *it);
+        itCards++;
+    }
+    delete this->orders;
+    this->orders = new list<TestOrders*>;
+    for(list<TestOrders*>::iterator it = player.orders->begin(); it != player.orders->end(); it++){
+        list<TestOrders*>::iterator itOrders = orders->begin();
+        this->orders->insert(itOrders, *it);
+        itOrders++;
+    }
 
     return *this;
 }
 
+// ostream operator
 ostream& operator<<(ostream &out, const Player &player){
     out << player.name;
     // for(map<int, Territory*>::iterator it = player.territories->begin(); it != player.territories->end(); it++){
@@ -49,11 +98,13 @@ ostream& operator<<(ostream &out, const Player &player){
     return out;
 }
 
+// accessor for territories
 map<int, Territory*>* Player::getTerritories() {
     return this->territories;
 }
 
-//return and display all territories that can be defended
+
+// return a list of territories that can be defended by the player
 map<int, Territory*>* Player::toDefend(){
     map<int, Territory*> *territoriesToDefend = new map<int, Territory*>;
     for(map<int,Territory*>::iterator it = territories->begin(); it != territories->end(); it++){
@@ -62,7 +113,8 @@ map<int, Territory*>* Player::toDefend(){
     return territoriesToDefend;
 }
 
-//return and display all territories that can be attacked
+
+// return a list of territories that can be attacked by the player
 map<int, Territory*>* Player::toAttack(list<Edge*> &edges){
     map<int, Territory*>* temp = new map<int, Territory*>;
     for(list<Edge*>::iterator itEdges = edges.begin(); itEdges != edges.end(); itEdges++){
@@ -76,6 +128,7 @@ map<int, Territory*>* Player::toAttack(list<Edge*> &edges){
     return temp;
 }
 
+// inserts an order to the player's list of orders(not tested)
 void Player::issueOrder(TestOrders *order){
     (*orders).insert(orders->end(),order);
 }
--- Player/PlayerDriver.cpp
+++ Player/PlayerDriver.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main(){
+    // test territories
     Territory *Portugal = new Territory(1, 1, *(new string("Portugal")));
     Territory *Spain = new Territory(2, 1, *(new string("Spain")));
     Territory *France = new Territory(3, 1, *(new string("France")));
@@ -13,6 +14,7 @@ int main(){
     Territory *Belgium = new Territory(7, 1, *(new string("Belgium")));
     Territory *Netherlands = new Territory(8, 1, *(new string("Netherlands")));
 
+    // test edges of the territories
     Edge *PS = new Edge(*Portugal, *Spain);
     Edge *SF = new Edge(*Spain, *France);
     Edge *FG = new Edge(*France, *Germany);
@@ -25,6 +27,7 @@ int main(){
     Edge *GB = new Edge(*Germany, *Belgium);
     Edge *GN = new Edge(*Germany, *Netherlands);
 
+    // create a list of the edges
     list<Edge*> *edges = new list<Edge*>;
     list<Edge*>::iterator it = edges->begin();
     edges->insert(it, PS);
@@ -47,12 +50,11 @@ int main(){
     it++;
     edges->insert(it,GN);
 
-
+    // test players
     Player *playerOne = new Player("One");
     Player *playerTwo = new Player("Two");
-
-    playerTwo = playerOne;
-
+    
+    // insert France, Germany and Switzerland to player one's territories
     // playerOne->getTerritories()->insert(pair<int, Territory*>(Portugal->countryNumber, Portugal));
     // playerOne->getTerritories()->insert(pair<int, Territory*>(Spain->countryNumber, Spain));
     playerOne->getTerritories()->insert(pair<int, Territory*>(France->countryNumber, France));
@@ -62,48 +64,44 @@ int main(){
     // playerOne->getTerritories()->insert(pair<int, Territory*>(Belgium->countryNumber, Belgium));
     // playerOne->getTerritories()->insert(pair<int, Territory*>(Netherlands->countryNumber, Netherlands));
 
+    // test method Player::toDefend()
     map<int, Territory*> *territoriesToDefend = playerOne->toDefend();
-
     cout << "Player " << *playerOne << " returned list of method Player::toDefend(): " << endl;
-
     for(map<int,Territory*>::iterator it = territoriesToDefend->begin(); it != territoriesToDefend->end(); it++){
         cout << (it->second->name) << endl;
     }
-
+    cout << endl;
     delete(territoriesToDefend);
     territoriesToDefend = nullptr;
-
-    cout << endl;
  
-    cout << "Player " << (*playerOne) << " returned list of method::toAttack(): " << endl;
-
+    // test method Player::toAttack()
     map<int, Territory*> *temp = playerOne->toAttack(*edges);
-
+    cout << "Player " << (*playerOne) << " returned list of method::toAttack(): " << endl;
     for(map<int, Territory*>::iterator it = temp->begin(); it != temp->end(); it++){
         cout << (it->second->name) << endl;
     }
 
+
+    // test assignment operator
+    playerTwo = playerOne;
     delete playerOne;
 
     cout << "===========================" << endl;
+    cout << "Test assignment operator"<< endl;
 
+    // territories to defend for player two
     territoriesToDefend = playerTwo->toDefend();
-
     cout << "Player " << *playerTwo << " returned list of method Player::toDefend(): " << endl;
-
     for(map<int,Territory*>::iterator it = territoriesToDefend->begin(); it != territoriesToDefend->end(); it++){
         cout << (it->second->name) << endl;
     }
-
+    cout << endl;
     delete(territoriesToDefend);
     territoriesToDefend = nullptr;
 
-    cout << endl;
- 
-    cout << "Player " << (*playerTwo) << " returned list of method::toAttack(): " << endl;
-
+    // territories to attack for player two
+    cout << "Player " << *playerTwo << " returned list of method::toAttack(): " << endl;
     temp = playerTwo->toAttack(*edges);
-
     for(map<int, Territory*>::iterator it = temp->begin(); it != temp->end(); it++){
         cout << (it->second->name) << endl;
     }
