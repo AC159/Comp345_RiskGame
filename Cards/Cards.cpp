8376410adc401d419fe527f3f6b6569b1f287bc0
Anastassy Cap <anacap100@gmail.com>
2022-02-06 11:59:42 -0500

    created structure of part 4

--- Cards/Cards.cpp
+++ Cards/Cards.cpp
@@ -1,5 +1,3 @@
-//
-// Created by anastassy on 1/20/22.
-//
-
 #include "Cards.h"
+
+
--- Cards/Cards.h
+++ Cards/Cards.h
@@ -2,10 +2,130 @@
 #ifndef COMP345RISKGAME_CARDS_H
 #define COMP345RISKGAME_CARDS_H
 
+#include <iostream>
+#include <vector>
+#include <string>
 
-class Cards {
+using namespace std;
 
+class Card {
+public:
+
+    Card();
+    Card(const Card &card);
+
+    Card& operator=(const Card &card);
+    friend ostream& operator<<(const ostream &out, const Card &card);
+
+    void play();
+
+    ~Card();
 };
 
+
+// Card of type Bomb
+class Bomb : public Card {
+public:
+    Bomb();
+    Bomb(const Bomb &bomb);
+
+    Bomb& operator=(const Bomb &bomb);
+    friend ostream& operator<<(const ostream &out, const Bomb &bomb);
+
+    void play();
+
+    ~Bomb();
+};
+
+
+// Card of type Reinforcement
+class Reinforcement : public Card {
+public:
+    Reinforcement();
+    Reinforcement(const Reinforcement &r);
+
+    Reinforcement& operator=(const Reinforcement &r);
+    friend ostream& operator<<(const ostream &out, const Reinforcement &r);
+
+    void play();
+
+    ~Reinforcement();
+};
+
+
+// Card of type Blockade
+class Blockade : public Card {
+public:
+    Blockade();
+    Blockade(const Blockade &blockade);
+
+    Blockade& operator=(const Blockade &blockade);
+    friend ostream& operator<<(const ostream &out, const Blockade &blockade);
+
+    void play();
+
+    ~Blockade();
+};
+
+
+// Card of type Airlift
+class Airlift : public Card {
+public:
+    Airlift();
+    Airlift(const Airlift &airlift);
+
+    Airlift& operator=(const Airlift &airlift);
+    friend ostream& operator<<(const ostream &out, const Airlift &airlift);
+
+    void play();
+
+    ~Airlift();
+};
+
+
+// Card of type Diplomacy
+class Diplomacy : public Card {
+public:
+    Diplomacy();
+    Diplomacy(const Diplomacy &diplomacy);
+
+    Diplomacy& operator=(const Diplomacy &diplomacy);
+    friend ostream& operator<<(const ostream &out, const Diplomacy &diplomacy);
+
+    void play();
+
+    ~Diplomacy();
+};
+
+
+class Hand {
+public:
+    vector<Card*> cards;
+
+    Hand();
+    Hand(const Hand &hand);
+
+    Hand& operator=(const Hand &hand);
+    friend ostream& operator<<(const ostream &out, const Hand &hand);
+
+    ~Hand();
+};
+
+
+class Deck {
+public:
+    vector<Card*> cards;
+
+    Deck();
+    Deck(const Deck &deck);
+
+    Deck& operator=(const Deck &deck);
+    friend ostream& operator<<(const ostream &out, const Deck &deck);
+
+    void draw();
+
+    ~Deck();
+
+};
 
 #endif //COMP345RISKGAME_CARDS_H
