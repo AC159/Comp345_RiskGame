e6e4d1bb5f8f191a0f85d6354cae599395dce47d
pop-cm <popcm@live.ca>
2022-02-11 07:23:58 -0500

    Cleanup and comment code, and add needed getters.

--- Orders/Orders.cpp
+++ Orders/Orders.cpp
@@ -3,27 +3,21 @@
 #include <iostream>
 using namespace std;
 
-// TODO: add more comments and remove dead code
 // ====================== Order class ======================
 Order::Order() {
     cout << "Created an Order base class. ";
 }
 
-Order::Order(const Order &order) {
-    //copy constructor is currently empty because class has no attributes yet
-}
+//copy constructor is currently empty because class has no attributes yet
+Order::Order(const Order &order) = default;
 
-Order & Order::operator=(const Order &) {
-    return *this;
-}
+//creates deep copy via the assignment operator (currently no attributes to copy)
+Order & Order::operator=(const Order &) = default;
 
+//overloads the insertion operator with a basic string representation of the object
 ostream & operator<<(ostream &out, const Order &order) {
     out << "Order object";
     return out;
-}
-
-istream & operator>>(istream &in, const Order &order) {
-    return in;
 }
 
 // ====================== Deploy class ======================
@@ -35,36 +29,38 @@ Deploy::Deploy(const Deploy &deploy) {
     //copy constructor is currently empty because class has no attributes yet
 }
 
+//creates deep copy via the assignment operator (currently no attributes to copy)
 Deploy & Deploy::operator=(const Deploy &deploy) {
     return *this;
 }
 
+//overloads the insertion operator with a basic string representation of the object
 ostream & operator<<(ostream &out, const Deploy &deploy) {
     out << "Deploy order object";
     return out;
 }
 
-istream & operator>>(istream &in, const Deploy &deploy) {
-    return in;
-}
-
+//returns whether the order is valid - behaviour is arbitrary for now
 bool Deploy::validate() {
-    cout << "Validated a Deploy order." << endl;
+    cout << "Validated a Deploy order.";
     return true;
 }
 
+//performs the order action if it's valid - action is arbitrary for now
 void Deploy::execute() {
     if (this->validate()) {
-        cout << "Executed a Deploy Order." << endl;
+        cout << " Executed a Deploy Order." << endl;
     } else {
         cout << "Invalid order could not be executed." << endl;
     }
 }
 
-string Deploy::className() {
+//polymorphically returns the name of the calling class
+string Deploy::className() const {
     return "Deploy";
 }
 
+//polymorphycally returns a clone of the calling class
 Deploy * Deploy::clone() const {
     return new Deploy(*this);
 }
@@ -78,36 +74,38 @@ Advance::Advance(const Advance &advance) {
     //copy constructor is currently empty because class has no attributes yet
 }
 
+//creates deep copy via the assignment operator (currently no attributes to copy)
 Advance &Advance::operator=(const Advance &advance) {
     return *this;
 }
 
+//overloads the insertion operator with a basic string representation of the object
 ostream & operator<<(ostream &out, const Advance &advance) {
     out << "Advance order object";
     return out;
 }
 
-istream & operator>>(istream &in, const Advance &advance) {
-    return in;
-}
-
+//returns whether the order is valid - behaviour is arbitrary for now
 bool Advance::validate() {
-    cout << "Validated an Advance order." << endl;
+    cout << "Validated an Advance order.";
     return true;
 }
 
+//performs the order action if it's valid - action is arbitrary for now
 void Advance::execute() {
     if (this->validate()) {
-        cout << "Executed an Advance Order." << endl;
+        cout << " Executed an Advance Order." << endl;
     } else {
         cout << "Invalid order could not be executed." << endl;
     }
 }
 
-string Advance::className() {
+//polymorphically returns the name of the calling class
+string Advance::className() const {
     return "Advance";
 }
 
+//polymorphycally returns a clone of the calling class
 Advance * Advance::clone() const {
     return new Advance(*this);
 }
@@ -121,36 +119,38 @@ Bomb::Bomb(const Bomb &bomb) {
     //copy constructor is currently empty because class has no attributes yet
 }
 
+//creates deep copy via the assignment operator (currently no attributes to copy)
 Bomb &Bomb::operator=(const Bomb &bomb) {
     return *this;
 }
 
+//overloads the insertion operator with a basic string representation of the object
 ostream & operator<<(ostream &out, const Bomb &bomb) {
     out << "Bomb order object";
     return out;
 }
 
-istream & operator>>(istream &in, const Bomb &bomb) {
-    return in;
-}
-
+//returns whether the order is valid - behaviour is arbitrary for now
 bool Bomb::validate() {
-    cout << "Validated a Bomb order." << endl;
+    cout << "Validated a Bomb order.";
     return true;
 }
 
+//performs the order action if it's valid - action is arbitrary for now
 void Bomb::execute() {
     if (this->validate()) {
-        cout << "Executed a Bomb Order." << endl;
+        cout << " Executed a Bomb order." << endl;
     } else {
         cout << "Invalid order could not be executed." << endl;
     }
 }
 
-string Bomb::className() {
+//polymorphically returns the name of the calling class
+string Bomb::className() const {
     return "Bomb";
 }
 
+//polymorphycally returns a clone of the calling class
 Bomb * Bomb::clone() const {
     return new Bomb(*this);
 }
@@ -164,36 +164,38 @@ Blockade::Blockade(const Blockade &blockade) {
     //copy constructor is currently empty because class has no attributes yet
 }
 
+//creates deep copy via the assignment operator (currently no attributes to copy)
 Blockade &Blockade::operator=(const Blockade &blockade) {
     return *this;
 }
 
+//overloads the insertion operator with a basic string representation of the object
 ostream & operator<<(ostream &out, const Blockade &blockade) {
     out << "Blockade order object";
     return out;
 }
 
-istream & operator>>(istream &in, const Blockade &blockade) {
-    return in;
-}
-
+//returns whether the order is valid - behaviour is arbitrary for now
 bool Blockade::validate() {
-    cout << "Validated a Blockade order." << endl;
+    cout << "Validated a Blockade order.";
     return true;
 }
 
+//performs the order action if it's valid - action is arbitrary for now
 void Blockade::execute() {
     if (this->validate()) {
-        cout << "Executed a Blockade Order." << endl;
+        cout << " Executed a Blockade order." << endl;
     } else {
         cout << "Invalid order could not be executed." << endl;
     }
 }
 
-string Blockade::className() {
+//polymorphically returns the name of the calling class
+string Blockade::className() const {
     return "Blockade";
 }
 
+//polymorphycally returns a clone of the calling class
 Blockade * Blockade::clone() const {
     return new Blockade(*this);
 }
@@ -207,36 +209,38 @@ Airlift::Airlift(const Airlift &airlift) {
     //copy constructor is currently empty because class has no attributes yet
 }
 
+//creates deep copy via the assignment operator (currently no attributes to copy)
 Airlift &Airlift::operator=(const Airlift *airlift) {
     return *this;
 }
 
+//overloads the insertion operator with a basic string representation of the object
 ostream & operator<<(ostream &out, const Airlift &airlift) {
     out << "Airlift order object";
     return out;
 }
 
-istream & operator>>(istream &in, const Airlift &airlift) {
-    return in;
-}
-
+//returns whether the order is valid - behaviour is arbitrary for now
 bool Airlift::validate() {
-    cout << "Validated an Airlift order." << endl;
+    cout << "Validated an Airlift order.";
     return true;
 }
 
+//performs the order action if it's valid - action is arbitrary for now
 void Airlift::execute() {
     if (this->validate()) {
-        cout << "Executed an Airlift Order." << endl;
+        cout << " Executed an Airlift Order." << endl;
     } else {
         cout << "Invalid order could not be executed." << endl;
     }
 }
 
-string Airlift::className() {
+//polymorphically returns the name of the calling class
+string Airlift::className() const {
     return "Airlift";
 }
 
+//polymorphycally returns a clone of the calling class
 Airlift * Airlift::clone() const {
     return new Airlift(*this);
 }
@@ -250,36 +254,38 @@ Negotiate::Negotiate(const Negotiate &negotiate) {
     //copy constructor is currently empty because class has no attributes yet
 }
 
+//creates deep copy via the assignment operator (currently no attributes to copy)
 Negotiate & Negotiate::operator=(const Negotiate *negotiate) {
     return *this;
 }
 
+//overloads the insertion operator with a basic string representation of the object
 ostream &operator<<(ostream &out, const Negotiate &negotiate) {
     out << "Negotiate order object";
     return out;
 }
 
-istream &operator>>(istream &in, const Negotiate &negotiate) {
-    return in;
-}
-
+//returns whether the order is valid - behaviour is arbitrary for now
 bool Negotiate::validate() {
-    cout << "Validated a Negotiate order." << endl;
+    cout << "Validated a Negotiate order.";
     return true;
 }
 
+//performs the order action if it's valid - action is arbitrary for now
 void Negotiate::execute() {
     if (this->validate()) {
-        cout << "Executed a Negotiate Order." << endl;
+        cout << " Executed a Negotiate Order." << endl;
     } else {
         cout << "Invalid order could not be executed." << endl;
     }
 }
 
-string Negotiate::className() {
+//polymorphically returns the name of the calling class
+string Negotiate::className() const {
     return "Negotiate";
 }
 
+//polymorphycally returns a clone of the calling class
 Negotiate * Negotiate::clone() const {
     return new Negotiate(*this);
 }
@@ -289,20 +295,22 @@ OrdersList::OrdersList() {
     cout << "Created an OrdersList object." << endl;
 }
 
+//creates a deep copy of the given OrdersList and of all the Order objects it contains
 OrdersList::OrdersList(const OrdersList &ordersList) {
-    // deep copy
     orders.reserve(ordersList.orders.size());
     for (Order *order: ordersList.orders) {
         orders.push_back(order->clone());
     }
 }
 
+//destructor deallocates memory to prevent memory leaks
 OrdersList::~OrdersList() {
     for (Order *order : orders) {
         delete order;
     }
 }
 
+//creates true deep copy via the assignment operator
 OrdersList & OrdersList::operator=(const OrdersList &ordersList) {
     if (this != &ordersList) {
         // deallocate memory from lhs
@@ -320,9 +328,10 @@ OrdersList & OrdersList::operator=(const OrdersList &ordersList) {
     return *this;
 }
 
+//overloads the insertion operator with a basic string representation of the object
 ostream & operator<<(ostream &out, const OrdersList &ordersList) {
-    out << "OrderList object:\torders {";
-    if (ordersList.orders.size() > 0) {
+    out << "{";
+    if (!ordersList.orders.empty()) {
         out << "[0]=" << ordersList.orders[0]->className();
         for (int i = 1; i < ordersList.orders.size(); i++) {
             out << ", [" << i << "]=" << ordersList.orders[i]->className();
@@ -332,23 +341,24 @@ ostream & operator<<(ostream &out, const OrdersList &ordersList) {
     return out;
 }
 
-istream & operator>>(istream &in, const OrdersList &ordersList) {
-    return in;
-}
-
-void OrdersList::add(Order *const newOrder) {
-    this->orders.push_back(newOrder);
-}
-
+//removes the element in the list located at the given index
 void OrdersList::remove(int index) {
+    //validate index
+    if (index >= orders.size()) {
+        return;
+    }
+
     delete orders[index];
     orders.erase(orders.begin() + index);
 }
 
+//moves the element from the given oldIndex to the given newIndex while preserving the order of other elements
 void OrdersList::move(int oldIndex, int newIndex) {
+    //validate indices
     if (oldIndex < 0 || newIndex < 0 || oldIndex == newIndex || oldIndex >= orders.size() || newIndex >= orders.size()) {
         return;
     }
+
     if (oldIndex < newIndex) {
         auto beginIt = orders.begin();
         rotate(beginIt + oldIndex, beginIt + oldIndex + 1, beginIt + newIndex + 1);
@@ -356,4 +366,24 @@ void OrdersList::move(int oldIndex, int newIndex) {
         auto rendIt = orders.rend();
         rotate(rendIt - oldIndex - 1, rendIt - oldIndex, rendIt - newIndex);
     }
+}
+
+//adds an element to the back of the list
+void OrdersList::add(Order *const newOrder) {
+    orders.push_back(newOrder);
+}
+
+//returns the current number of elements in the list
+size_t OrdersList::length() const{
+    return orders.size();
+}
+
+//returns the element at the given index
+Order * OrdersList::element(int index) const {
+    //validate index
+    if (index >= orders.size()) {
+        return nullptr;
+    }
+
+    return orders[index];
 }
