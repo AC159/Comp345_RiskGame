3ceceb10377be8c54fd76b2757c761792221ae3b
Anastassy Cap <anacap100@gmail.com>
2022-04-02 09:39:38 -0400

    implemented structure of Benevolent player

--- PlayerStrategies/PlayerStrategies.cpp
+++ PlayerStrategies/PlayerStrategies.cpp
@@ -10,3 +10,18 @@ PlayerStrategies::PlayerStrategies(const PlayerStrategies &ps) {
 }
 
 PlayerStrategies::~PlayerStrategies() = default;
+
+Benevolent::Benevolent(Players::Player *p) : PlayerStrategies(p) {}
+
+Benevolent::Benevolent(const Benevolent &b) : PlayerStrategies(b.player) {}
+
+Benevolent& Benevolent::operator=(const Benevolent &b) {
+    this->player = b.player;
+}
+
+std::ostream &operator<<(std::ostream &out, const Benevolent &b) {
+    out << "Player " << b.player->getName() << " is playing the Benevolent strategy" << std::endl;
+    return out;
+}
+
+Benevolent::~Benevolent() = default;
--- PlayerStrategies/PlayerStrategies.h
+++ PlayerStrategies/PlayerStrategies.h
@@ -11,9 +11,8 @@
 
 
 class PlayerStrategies {
-protected:
-    Players::Player *player;
 public:
+    Players::Player *player;
     virtual std::multimap<int, Graph::Territory *, std::greater<>> toDefend(const std::vector<Graph::Edge *> &mapEdges) = 0;
     virtual std::multimap<int, Graph::Territory *> toAttack(const std::vector<Graph::Edge *> &edges) = 0;
     virtual void issueOrder(Cards::Deck *deck, Graph::Map *map) = 0;
@@ -23,5 +22,19 @@ public:
     virtual ~PlayerStrategies();
 };
 
+
+class Benevolent : public PlayerStrategies {
+public:
+    std::multimap<int, Graph::Territory *, std::greater<>> toDefend(const std::vector<Graph::Edge *> &mapEdges);
+    std::multimap<int, Graph::Territory *> toAttack(const std::vector<Graph::Edge *> &edges);
+    void issueOrder(Cards::Deck *deck, Graph::Map *map);
+
+    Benevolent& operator=(const Benevolent &b);
+    friend std::ostream& operator<<(std::ostream &out, const Benevolent &b);
+
+    Benevolent(Players::Player *p);
+    Benevolent(const Benevolent &b);
+    ~Benevolent() override;
+};
 
 #endif //COMP345RISKGAME_PLAYERSTRATEGIES_H
